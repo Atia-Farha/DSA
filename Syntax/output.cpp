14b322c990e4ea8b5bee2838a60d80771a21e899
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-19 23:15:02 +0600

    Update output.cpp

--- Syntax/output.cpp
+++ Syntax/output.cpp
@@ -3,4 +3,6 @@ cout << "Hello World!";
 
 // Output with newline
 cout << "Hello World!\n"; // Faster method
-cout << "Hello World!" << endln; // Slower method
+cout << "Hello World!" << endl; // Slower method
+
+cout << "Hello World!" << "\n";
