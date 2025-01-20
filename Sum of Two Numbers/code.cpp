f688f03283ed38b393c4bc9282231f4223d71429
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-20 15:41:24 +0600

    Update

---
+++ Sum of Two Numbers/code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, sum;
+
+  cout << "Enter the first number: ";
+  cin >> a;
+
+  cout << "Enter the second number: ";
+  cin >> b;
+
+  sum = a + b;
+
+  cout << "Sum of two numbers: " << sum << "\n";
+
+  return 0;
+}
