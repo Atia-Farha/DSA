78d8227b650a7a965ba52f2273ec576f1f046a21
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-24 14:01:04 +0600

    Update code.cpp

--- Problem Solving/All odd numbers from 1 to n/code.cpp
+++ Problem Solving/All odd numbers from 1 to n/code.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main() {
   int n;
+  cout << "Enter the value of n: ";
   cin >> n;
 
+  cout << "All odd numbers from 1 to " << n << "= ";
   for(int i=1; i<=n; i++) {
     if(i%2 != 0) {
       cout << i << " ";
