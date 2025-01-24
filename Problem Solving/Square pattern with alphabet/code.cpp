f268bd17f7fcee61673dd088c6f2e214564a041b
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-24 15:24:51 +0600

    Update Problem Solving

---
+++ Problem Solving/Square pattern with alphabet/code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  
+  cout << "Enter the length of square: ";
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    char ch = 'A';
+
+    for (int j = 0; j < n; j++) {
+      cout << ch << " ";
+      ch += 1;
+    }
+
+    cout << "\n";
+  }
+
+  return 0;
+}
