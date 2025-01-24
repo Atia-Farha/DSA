bc15b5706f696bda748779b64cecabd774e94b3d
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-24 14:38:48 +0600

    Update Problem Solving

---
+++ Problem Solving/Check if a number is prime or not/code.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  bool prime = true;
+
+  cout << "Enter the number: ";
+  cin >> n;
+
+  if(n < 2) { 
+    prime = false;
+  }
+  else {
+    for(int i = 2; i * i <= n; i++) {
+      if(n % i == 0) {
+        prime = false;
+        break;
+      }
+    }
+  }
+
+  if(prime) {
+    cout << n << " is a prime number.\n";
+  }
+  else {
+    cout << n << " is a non-prime number.\n";
+  }
+
+  return 0;
+}
