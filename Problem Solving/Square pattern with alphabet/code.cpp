4187af13bdfd712ea71ea7501c526acffd52ec77
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-24 15:51:37 +0600

    Update code.cpp

--- Problem Solving/Square pattern with alphabet/code.cpp
+++ Problem Solving/Square pattern with alphabet/code.cpp
@@ -9,12 +9,10 @@ int main() {
 
   for (int i = 0; i < n; i++) {
     char ch = 'A';
-
     for (int j = 0; j < n; j++) {
       cout << ch << " ";
-      ch += 1;
+      ch++;
     }
-
     cout << "\n";
   }
 
