c43e5667476bdca443403850877eafc6134f9175
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-25 16:52:58 +0600

    Update cp-template.cpp

--- cp-template.cpp
+++ cp-template.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 typedef long long ll;
-typedef ull unsigned long long;
+typedef unsigned long long ull;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef vector<int> vi;
@@ -10,9 +10,9 @@ typedef vector<ll> vl;
 typedef vector<pii> vpii;
 typedef vector<pll> vpll;
 
-#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
 #define REP(i, n) FOR(i, 0, n)
-#define foreach(a, x) for(auto& a : x)
+#define foreach(a, x) for (auto& a : x)
 #define all(x) begin(x), end(x)
 #define rall(x) x.rbegin(), x.rend()
 #define sz(x) (int)(x).size()
@@ -27,13 +27,48 @@ typedef vector<pll> vpll;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e18;
 
-ll modpow(ll base, ll exp) {
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+ll modadd(ll a, ll b, ll m = MOD) {
+    return ((a % m + b % m) % m + m) % m;
+}
+ll modsub(ll a, ll b, ll m = MOD) {
+    return ((a % m - b % m) % m + m) % m;
+}
+ll modmul(ll a, ll b, ll m = MOD) {
+    return ((a % m) * (b % m)) % m;
+}
+ll modpow(ll base, ll exp, ll m = MOD) {
     ll res = 1;
-    for(; exp > 0; exp >>= 1) {
-        if(exp & 1) res = (res * base) % MOD;
-        base = (base * base) % MOD;
+    for (; exp > 0; exp >>= 1) {
+        if (exp & 1) res = (res * base) % m;
+        base = (base * base) % m;
     }
     return res;
+}
+
+// Debugging tools (remove for submission)
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int x) { cerr << x; }
+void _print(long long x) { cerr << x; }
+void _print(string x) { cerr << x; }
+void _print(char x) { cerr << x; }
+template <class T> void _print(vector<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
 }
 
 void solve() {
@@ -41,9 +76,7 @@ void solve() {
 }
 
 int main() {
-    fastio;
-    
-    
-
-    return 0;
-}
+       fastio;
+       solve();
+       return 0;
+   }
