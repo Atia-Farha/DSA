fe8e3de5aab2ee33c68d4523ed9020a0394b5bd8
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-25 16:53:32 +0600

    Update cp-template.cpp

--- cp-template.cpp
+++ cp-template.cpp
@@ -22,7 +22,7 @@ typedef vector<pll> vpll;
 #define se second
 
 #define endl '\n'
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
 
 const ll MOD = 1e9 + 7;
 const ll INF = 1e18;
