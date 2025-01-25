ce48db538e16d0834f7c58d3b72f2cf0cc9ddf7c
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-25 17:08:55 +0600

    Update cp-template.cpp

--- cp-template.cpp
+++ cp-template.cpp
@@ -10,7 +10,7 @@ typedef vector<ll> vl;
 typedef vector<pii> vpii;
 typedef vector<pll> vpll;
 
-#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FOR(i, a, b) for (int i = a; i < (b); ++i)
 #define REP(i, n) FOR(i, 0, n)
 #define foreach(a, x) for (auto& a : x)
 #define all(x) begin(x), end(x)
@@ -27,59 +27,90 @@ typedef vector<pll> vpll;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e18;
 
-ll gcd(ll a, ll b) {
-    while(b) { a %= b; swap(a, b); }
-    return a;
-}
-ll lcm(ll a, ll b) {
-    return a / gcd(a, b) * b;
-}
-ll modadd(ll a, ll b) {
-    return ((a%MOD + b%MOD) % MOD + MOD) % MOD;
+ll modadd(ll a, ll b, ll m = MOD) {
+    return ((a % m) + (b % m) + m) % m;
 }
 ll modsub(ll a, ll b, ll m = MOD) {
-    return ((a % m - b % m) % m + m) % m;
+    return ((a % m) - (b % m) + m) % m;  // Ensures non-negative result
 }
 ll modmul(ll a, ll b, ll m = MOD) {
     return ((a % m) * (b % m)) % m;
 }
 ll modpow(ll base, ll exp, ll m = MOD) {
     ll res = 1;
+    base %= m;
     for (; exp > 0; exp >>= 1) {
         if (exp & 1) res = (res * base) % m;
         base = (base * base) % m;
     }
     return res;
 }
+ll modinv(ll a, ll m = MOD) {
+    return modpow(a, m - 2, m);
+}
+ll gcd(ll a, ll b) {
+    while(b) { a %= b; swap(a, b); }
+    return a;
+}
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
 
-// Debugging tools (remove for submission)
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#define DEBUG
+#ifdef DEBUG
+    #define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
 #else
-#define debug(x)
+    #define debug(x)
 #endif
 
 void _print(int x) { cerr << x; }
-void _print(long long x) { cerr << x; }
-void _print(string x) { cerr << x; }
+void _print(ll x) { cerr << x; }
+void _print(ull x) { cerr << x; }
 void _print(char x) { cerr << x; }
-void _print(pii x) { cerr << "(" << x.fi << ", " << x.se << ")"; }
+void _print(string x) { cerr << x; }
 void _print(bool x) { cerr << (x ? "true" : "false"); }
-template <class T> void _print(vector<T> v) {
+void _print(pii x) { cerr << "(" << x.fi << ", " << x.se << ")"; }
+
+template <class T>
+void _print(vector<T> v) {
     cerr << "[ ";
-    for (T i : v) {
+    for (const auto& i : v) {
         _print(i);
         cerr << " ";
     }
     cerr << "]";
 }
 
+template <class T>
+void _print(vector<vector<T>> v) {
+    cerr << "[ ";
+    for (const auto& row : v) {
+        _print(row);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+// Uncomment for unordered_map<pair<int, int>, ...> support
+/*
+struct pair_hash {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const {
+        auto h1 = hash<T1>{}(p.first);
+        auto h2 = hash<T2>{}(p.second);
+        return h1 ^ (h2 << 1);
+    }
+};
+*/
+
 void solve() {
     
 }
 
 int main() {
-       fastio;
-       solve();
-       return 0;
+    fastio;
+
+    solve();
+
+    return 0;
 }
