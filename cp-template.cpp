21a3e0d32402be3780a832ea7fed0b6682698721
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-25 15:43:15 +0600

    Update

---
+++ cp-template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define REP(i, n) FOR(i, 0, n)
+
+ll modpow(ll base, ll exp) {
+    ll res = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % MOD;
+        base = (base * base) % MOD;
+        exp /= 2;
+    }
+    return res;
+}
+
+int main() {
+    fastio;
+    
+}
