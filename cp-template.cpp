1e94c9cdc692d5aa468b8e75e9c45cf84def4b42
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-25 16:07:59 +0600

    Update cp-template.cpp

--- cp-template.cpp
+++ cp-template.cpp
@@ -2,15 +2,29 @@
 using namespace std;
 
 typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
 
+#define for(i, a, b) for(int i = a; i < (b); ++i)
+#define rep(i, n) for(i, 0, n)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define trav(a, x) for(auto& a : x)
+
+// I/O optimization
 #define endl '\n'
 #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
 const int MOD = 1e9 + 7;
 const ll INF = 1e18;
-
-#define FOR(i, a, b) for (int i = a; i < b; i++)
-#define REP(i, n) FOR(i, 0, n)
 
 ll modpow(ll base, ll exp) {
     ll res = 1;
