cf01755c2b24e537939707f183540d858c2f5578
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-28 01:19:12 +0600

    Update code.cpp

--- Problem Solving/GCD/code.cpp
+++ Problem Solving/GCD/code.cpp
@@ -1,22 +1,37 @@
-// Euclid's Algorithm (Iterative Version)
 #include <bits/stdc++.h>
 using namespace std;
 
 #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
 #define endl '\n'
 
-int main() {
-    fastio;
+ll gcd(ll a, ll b) {
+    // Handle zero cases
+    if (a == 0) return abs(b);
+    if (b == 0) return abs(a);
 
-    int a, b;
-    cin >> a >> b;
+    // Ensure non-negative inputs
+    a = abs(a);
+    b = abs(b);
 
+    // Early return if a == b
+    if (a == b) return a;
+
+    // Iterative Euclid's Algorithm
     while (b != 0) {
-        int temp = b;
+        ll temp = b;
         b = a % b;
         a = temp;
     }
-    cout << a << endl;
+    return a;
+}
+
+int main() {
+    fastio;
+
+    long long a, b;
+    cin >> a >> b;
+
+    cout << gcd(a, b) << endl;
 
     return 0;
 }
