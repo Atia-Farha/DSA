09bf29fa46ecd9e21004677072bd8e508d16ca36
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-28 01:21:35 +0600

    Update code.cpp

--- Problem Solving/GCD/code.cpp
+++ Problem Solving/GCD/code.cpp
@@ -1,3 +1,4 @@
+// Euclid's Algorithm (Iterative Version)
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,23 +6,20 @@ using namespace std;
 #define endl '\n'
 
 ll gcd(ll a, ll b) {
-    // Handle zero cases
     if (a == 0) return abs(b);
     if (b == 0) return abs(a);
 
-    // Ensure non-negative inputs
     a = abs(a);
     b = abs(b);
 
-    // Early return if a == b
     if (a == b) return a;
-
-    // Iterative Euclid's Algorithm
+ 
     while (b != 0) {
         ll temp = b;
         b = a % b;
         a = temp;
     }
+
     return a;
 }
 
