c131e94be89988a4e7a825cc50d786b84bcf2315
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-01-28 12:51:06 +0600

    Update code.cpp

--- Problem Solving/Factorial of a number/code.cpp
+++ Problem Solving/Factorial of a number/code.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
 const int MOD = 1e9 + 7;
 
-int factorial(int n) {
+using ll = long long;
+ 
+ll factorial(ll n) {
     if (n == 0 || n == 1) {
         return 1;
     }
 
-    long long result = 1;
-    for (int i = 2; i <= n; ++i) {
+    ll result = 1;
+    for (ll i = 2; i <= n; ++i) {
         result = (result * i) % MOD;
     }
     return result;
 }
 
 int main() {
-    int n;
+    ll n;
     cin >> n;
 
     cout << factorial(n) << endl;
