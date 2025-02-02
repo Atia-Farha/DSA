9b2214d98d934a855e2cca90df852c17be3cacdd
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-02-02 18:40:23 +0600

    Update Problem Solving

---
+++ Problem Solving/Divisors calculation with sorting/code.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+int main() {
+    fastio;
+
+    int A;
+    cin >> A;
+
+    if (A == 1) {
+        cout << "1" << endl;
+        return 0;
+    }
+
+    vector<int> smallDivisors;
+    stack<int> largeDivisors; 
+
+    for (int i = 1; i * i <= A; i++) {
+        if (A % i == 0) {
+            smallDivisors.push_back(i);
+            if (i != A / i) {
+                largeDivisors.push(A / i);
+            }
+        }
+    }
+
+    for (int d : smallDivisors) {
+        cout << d << '\n';
+    }
+
+    while (!largeDivisors.empty()) {
+        cout << largeDivisors.top() << endl;
+        largeDivisors.pop();
+    }
+
+    return 0;
+}
