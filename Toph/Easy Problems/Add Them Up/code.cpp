69aeac906606641dd8a01a3674964c54ab69b3d0
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-02-10 02:40:56 +0600

    Update Easy Problems

---
+++ Toph/Easy Problems/Add Them Up/code.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+int main() {
+    fastio;
+
+    int x, y;
+    cin >> x >> y;
+	   cout << x+y << endl;
+    return 0;
+}
