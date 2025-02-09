37d39297750419f9d79c09389c0a747ad23a7875
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-02-10 02:39:23 +0600

    Update

---
+++ Toph/Easy Problems/Copycat/code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+int main() {
+    fastio;
+
+    int x;
+	   cin >> x;
+    cout << x << endl;
+
+	   return 0;
+}
