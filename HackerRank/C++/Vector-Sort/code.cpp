d1dbde67aa42980600d2899665d5018adf30a25f
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-02-19 10:29:56 +0600

    Update C++

---
+++ HackerRank/C++/Vector-Sort/code.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+int main() {
+    fastio;
+
+    int N;
+    cin >> N;
+
+    vector<int> arr(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
