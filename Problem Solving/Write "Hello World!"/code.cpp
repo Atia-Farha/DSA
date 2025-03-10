cc7f04e455f573c288e0efdbea8af6c67676245c
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-03-10 19:41:28 +0600

    Update code.cpp

--- Problem Solving/Write "Hello World!"/code.cpp
+++ Problem Solving/Write "Hello World!"/code.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
 int main() {
-  cout << "Hello World!\n";
-  return 0;
+    fastio;
+
+    cout << "Hello World!\n"; 
+
+    return 0;
 }
