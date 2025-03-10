f5316a0a3cee6032f79488b757dbfa188c9186e2
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-03-10 20:45:37 +0600

    Create Conversion from Decimal to Binary/code.cpp

---
+++ Problem Solving/Conversion from Decimal to Binary/code.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+int main() {
+    fastio;
+
+    int d, r, b = 0, pow = 1;
+    cin >> d;
+
+    while(d > 0) {
+        r = d % 2;
+        d /= 2;
+        b += ( r * pow);
+        pow *= 10;
+    }
+
+    cout << b << endl;
+
+    return 0;
+}
