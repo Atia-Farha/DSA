79db9118996c01def6342111aa882f631b71d7e2
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-03-11 21:14:15 +0600

    Create short.cpp

---
+++ Problem Solving/Find out the smallest value inside an array/short.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+int main() {
+    fastio;
+
+    int arr[] = {5, 15, 22, 1, -15, -24};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int smallest = INT_MAX;
+
+    for(int i = 0; i < size; ++i) {
+        smallest = min(arr[i], smallest);
+    }
+
+    cout << smallest << endl;
+
+    return 0;
+}
