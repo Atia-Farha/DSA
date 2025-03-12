6790757208f31bd2a697e38127bc6333157bc742
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-03-12 12:46:18 +0600

    Create Reverse an array (2 Pointer)/code.cpp

---
+++ Problem Solving/Reverse an array (2 Pointer)/code.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+template <typename T>
+void reverseArray(T arr[], int size) {
+    int start = 0, end = size - 1;
+
+    while(start < end) {
+        swap(arr[start], arr[end]);
+        ++start;
+        --end;
+    }
+}
+
+int main() {
+    fastio;
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, size);
+
+    for(int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
