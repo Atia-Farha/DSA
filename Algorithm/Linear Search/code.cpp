55514988cedb9f2a46276b3202f8a44e80264893
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-03-12 12:18:01 +0600

    Create Algorithm/Linear Search/code.cpp

---
+++ Algorithm/Linear Search/code.cpp
@@ -0,0 +1,29 @@
+# Time Complexity: O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+int linearSearch(int arr[], int size, int target) {
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == target) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    fastio;
+
+    int arr[] = {4, 2, 7, 8, 1, 2, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+
+    cout << linearSearch(arr, size, target) << endl;
+
+    return 0;
+}
