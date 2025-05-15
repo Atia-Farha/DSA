a068d3a8a7384f08f9f71d3e225b97e43ea9fc5a
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-05-15 23:01:53 +0600

    Update code.cpp

--- Codeforces/Ambitious Kid/code.cpp
+++ Codeforces/Ambitious Kid/code.cpp
@@ -1,5 +1,15 @@
 // https://codeforces.com/problemset/problem/1866/A
 
+// Note:
+// 1. Need to find for an array's element how many operations (+ or -) we need to do to reach 0.
+// 2. The element with the smallest possible operation is the answer.
+// Example: [2, -6, 5]
+//         i) -6 will need 6 operations to reach 0.
+//         ii) 5 will need 5.
+//         iii) 2 will need 2.
+//         So the answer is 2.
+// 3. We need to make negative numbers positive to make the process easier.
+
 #include <bits/stdc++.h>
 using namespace std;
 
