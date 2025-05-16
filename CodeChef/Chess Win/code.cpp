c0e5732e1b6c73680362e73f11ec99f9a5968daf
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-05-16 17:03:06 +0600

    Create Chess Win/code.cpp

---
+++ CodeChef/Chess Win/code.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/CHESSWIN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+int main() {
+    fastio;
+    
+    int a, b;
+    cin >> a >> b;
+    
+    cout << b - a + 1 << endl;
+    
+    return 0;
+}
