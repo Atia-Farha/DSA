262cabf6b3439db43b2ee7f92f487321998f408f
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-05-16 17:02:01 +0600

    Create CodeChef/Squid Game - Piggy Bank/code.cpp

---
+++ CodeChef/Squid Game - Piggy Bank/code.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/SQUIDBANK
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define endl '\n'
+
+int main() {
+    fastio;
+
+    long long n, k, p;
+    cin >> n >> k;
+
+    p = (n - k) * 10000;
+
+    cout << p << endl;
+
+    return 0;
+}
