606f62bb00229f001bfde8ad323fbfbb2dd67084
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-05-16 17:04:19 +0600

    Create Pizza Split/code.cpp

---
+++ CodeChef/Pizza Split/code.cpp
@@ -0,0 +1,23 @@
+// https://www.codechef.com/problems/PZSPLIT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define endl '\n'
+
+int main() {
+    fastio;
+
+    int n;
+    cin >> n;
+    
+    if (n % 2 == 0) {
+        cout << 1 << endl;
+    }
+    else {
+        cout << 2 << endl;
+    }
+    
+    return 0;
+}
